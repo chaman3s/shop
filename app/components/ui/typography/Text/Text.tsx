8a2c683fb6b8a91c303ff29350cdc2a61d99eca6
chaman3s <chamanggarwal@gmail.com>
2026-05-20 21:19:27 +0530

    fix text

--- app/components/ui/typography/Text/Text.tsx
+++ app/components/ui/typography/Text/Text.tsx
@@ -1,11 +1,17 @@
-import { JSX } from "react";
-import "./Text.css"
-type Textprops ={
-    text: string;
-    as : keyof JSX.IntrinsicElements;
-} & React.HTMLAttributes<HTMLElement>;
-export default function Text({text,as ,...props}:Textprops){
-    const Ele = as;
-    return (<Ele {...props}>{text}</Ele>)
-    
+import { ElementType, ComponentPropsWithoutRef } from "react";
+import "./Text.css";
+
+type TextProps<T extends ElementType> = {
+  text: string;
+  as: T;
+} & ComponentPropsWithoutRef<T>;
+
+export default function Text<T extends ElementType>({
+  text,
+  as,
+  ...props
+}: TextProps<T>) {
+  const Ele = as;
+
+  return <Ele {...props}>{text}</Ele>;
 }
